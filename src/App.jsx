ca87453b09987b44bf2eb2e346a2055a9d8cb942
Pranav-496 <pranavlandge79@gmail.com>
2026-01-19 22:48:46 +0530

    Creating the required pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router";
+import { Home } from "./pages/Home";
+import { CoinDetail } from "./pages/CoinDetail";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/coin/:id" element={<CoinDetail />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
