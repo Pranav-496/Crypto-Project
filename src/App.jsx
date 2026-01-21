1fb990b321f86eb43ea79ecf93da2b3fa2702619
Pranav-496 <pranavlandge79@gmail.com>
2026-01-21 17:12:30 +0530

    Final fixes and proper deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
 import { CoinDetail } from "./pages/CoinDetail";
 
 function App() {
   return (
-    <BrowserRouter basename="/Crypo-Project/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coin/:id" element={<CoinDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coin/:id" element={<CoinDetail />} />
+    </Routes>
   );
 }
 
